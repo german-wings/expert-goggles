ed9a9869593a2a3368a0808d262656d085e5eba4
Bhavar Shankar Kumavat <52790214+german-wings@users.noreply.github.com>
2022-10-19 17:16:26 +0530

    Making a Current Request Poll for every sequence number
    Not efficient at all because of lots of network overhead.

--- MTConnectCodeSamples/MTConnectMazakConnectionTest.js
+++ MTConnectCodeSamples/MTConnectMazakConnectionTest.js
@@ -4,24 +4,50 @@ import { JSDOM } from 'jsdom'
 import axios from 'axios'
 
 //sample api to connect mazak hosted mtconnect simulator
-//exhibits a simple connection process involving a probe - current - sample
+//exhibits a simple connection process involving a probe - current - sample.
+
+const target = "192.168.1.184:8082"
+//const target = "mtconnect.mazakcorp.com:5610"
 
 let wrapper = async () => {
-    const currentResponse = await axios.get(`http://mtconnect.mazakcorp.com:5609/current?path=//Controller`)
+    const currentResponse = await axios.get(`http://${target}/current?path=//Controller`)
     const currentResponseDOM = new JSDOM(currentResponse.data, { contentType: 'text/xml' })
     const listOfSequences = currentResponseDOM.window.document.querySelectorAll("[sequence]")
+    const header = currentResponseDOM.window.document.querySelector('Header')
+    const instanceId = header.getAttribute("instanceId")
+    const nextSequence = header.getAttribute("nextSequence")
+    const firstSequence = parseInt(header.getAttribute("firstSequence"))
+    let lastSequence = header.getAttribute("lastSequence")
 
-    let nextSequence = currentResponseDOM.window.document.querySelector("[nextSequence]")
+    let currentSequence = firstSequence
 
     while (true) {
-        const sampleResponse = await axios.get(`http://mtconnect.mazakcorp.com:5609/sample`)
+
+        const sampleResponse = await axios.get(`http://${target}/current?at=${currentSequence}&path=//Controller`)
         const sampleResponseDOM = new JSDOM(sampleResponse.data, { contentType: 'text/xml' })
-        const listOfSequences = sampleResponseDOM.window.document.querySelectorAll("[sequence]")
-        console.log(`Received ${listOfSequences.length}`)
-        nextSequence = sampleResponseDOM.window.document.querySelector("[nextSequence]")
+        
+        const header = sampleResponseDOM.window.document.querySelector('Header')
+        const sampleInstanceId = header.getAttribute("instanceId")
+        const sampleNextSequence = parseInt(header.getAttribute("nextSequence"))
+        const sampleLastSequence = parseInt(header.getAttribute("lastSequence"))
+        const runStatus = sampleResponseDOM.window.document.querySelector("[name='RunStatus']").textContent
+        const mode = sampleResponseDOM.window.document.querySelector("[name='Mode']").textContent
+        const program = sampleResponseDOM.window.document.querySelector("[name='Program']").textContent
+        const feedRateOverride = sampleResponseDOM.window.document.querySelector("[name='FeedrateOverride']").textContent
+
+        console.log(`LS ${sampleLastSequence} NS ${sampleNextSequence} CS ${currentSequence} RSTAT ${runStatus} MODE ${mode} PROGRAM ${program} FEED ${feedRateOverride}`)
+        currentSequence++
+        if(currentSequence >= sampleNextSequence){
+            currentSequence = sampleLastSequence
+        }
+
+        if(sampleInstanceId !== instanceId){
+            console.log("Instance ID Fault")
+        }
     }
 
 }
+
 
 
 wrapper()
