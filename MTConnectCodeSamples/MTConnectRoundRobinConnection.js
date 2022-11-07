c9c68b3a54a6621a20d70cf1097efa1f78dc0d30
bhavar kumavat <bhavarkumavat@yandex.com>
2022-11-07 08:28:42 +0530

    Multiple Request Broking example

---
+++ MTConnectCodeSamples/MTConnectRoundRobinConnection.js
@@ -0,0 +1,104 @@
+import axios from "axios"
+import { JSDOM } from "jsdom"
+
+const device_1 = {
+    'endpoint': 'http://mtconnect.mazakcorp.com:5610/',
+    'keyOfInterest': ['Program', 'ProgramComment', 'ControllerMode'],
+}
+
+const device_2 = {
+    'endpoint': 'http://mtconnect.mazakcorp.com:5611/',
+    'keyOfInterest': ['Program', 'ProgramComment', 'ControllerMode'],
+}
+
+const mtconnect_devices = [device_1, device_2]
+const localDeviceStateList = []
+
+
+/*
+//Sample Object for recording state across loop iterations.
+
+probeDone?true or false
+currentDone?true or false
+instanceID
+serialNumber
+currentSequence
+keys of interest {objects}
+endpoint
+nextSequence
+firstSequence
+lastSequence
+*/
+
+async function initiateMTConnectSequence() {
+
+    for (let counter = 0; counter < mtconnect_devices.length; counter += 1) {
+        console.log('--------------------------------------------------')
+        //check if the device has a probeRequest conducted?
+        let device = mtconnect_devices[counter]
+        if (device.probeDone == undefined || device.probeDone == false) {
+            console.log('Probing..')
+            //please conduct a probe request
+            let probeResponse = await axios.get(`${device.endpoint}probe`, { headers: { 'Accept': 'text/xml' } })
+            let dom = new JSDOM(probeResponse.data).window.document
+            let instanceID = dom.querySelector('[instanceId]').getAttribute('instanceId')
+            let serialNumber = dom.querySelector('[serialNumber]').getAttribute('serialNumber')
+            if (instanceID == null && serialNumber == null) {
+                throw "Probe Response inconsistent"
+            }
+            device.serialNumber = serialNumber
+            device.instanceID = instanceID
+            //mark the device state as true
+            device.probeDone = true
+        }
+
+        //request stage changes to current mode now
+        //check if the probeIsDone
+        //check if instanceID matches
+        //populate initial values
+
+        if (device.probeDone === true && device.instanceID != null) {
+
+            let currentRequestURL = null
+
+            //check if currentRequest is done ?
+            if (device.currentDone === true) {
+
+            }
+
+            let currentResponse = await axios.get(`${currentRequestURL}`, { headers: { 'Accept': 'text/xml' } })
+            let dom = new JSDOM(currentResponse.data).window.document
+            let instanceID = dom.querySelector('[instanceId]').getAttribute('instanceId')
+
+            //checking is instanceID matches if it doesnot skip using continue ! set probeDone false
+            if (instanceID !== device.instanceID) {
+                device.probeDone = false
+                console.log('Instance ID Mismatch')
+                continue
+            }
+            else {
+                //continue getting new values here
+                let firstSequence = dom.querySelector('[firstSequence]').getAttribute('firstSequence')
+                let nextSequence = dom.querySelector('[nextSequence]').getAttribute('nextSequence')
+                let lastSequence = dom.querySelector('[lastSequence]').getAttribute('lastSequence')
+
+                //populate state related keys
+                device.firstSequence = firstSequence
+                device.nextSequence = nextSequence
+                device.lastSequence = lastSequence
+
+                //sort all the sequences in ascending order
+                let sequences = dom.querySelectorAll('[sequence]')
+                let list_of_sequences = []
+                sequences.forEach((item) => list_of_sequences.push(item))
+                list_of_sequences.sort((a, b) => parseInt(a.getAttribute('sequence')) - parseInt(b.getAttribute('sequence')))
+
+            }
+        }
+    }
+
+}
+
+while (true) {
+    await initiateMTConnectSequence()
+}
