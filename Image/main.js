e78b514d648615152868b7a05b9684e7a3ca25e0
Bhavar Shankar Kumavat <52790214+german-wings@users.noreply.github.com>
2023-11-18 17:03:34 +0000

    Unstable

--- Image/main.js
+++ Image/main.js
@@ -12,25 +12,26 @@ const JSDOM = require('jsdom')
 
 //Some variables that will be in the enviroment via Docker Compose !
 const ENDPOINT_MAC = ""
-const BASE_URL = "http://mtconnect.mazakcorp.com:5610"
+const BASE_URL = "https://smstestbed.nist.gov/vds/GFAgie01"
 const ENDPOINT_SERIAL_NUMBER = ""
 
 
 //some global variables to use in the script
-const globalCurrentState = null
+var globalNextRequestType = 'probe'
 var globalClassifiedElements = {}
-var nextState = 'probe'
+var globalNextSequence = null
+var globalMachineState = null
 
 
-async function getXMLResponse(request){
+async function getXMLResponse(request) {
     const response = await axios(request)
-    if(response.status !== 200){
+    if (response.status !== 200) {
         throw Error(`Invalid HTTP Response /n ${response}`)
     }
 
-    const dom = new JSDOM.JSDOM(response.data , {contentType : 'text/html'})
+    const dom = new JSDOM.JSDOM(response.data, { contentType: 'application/xml' })
 
-    if(dom === null || dom === undefined){
+    if (dom === null || dom === undefined) {
         throw Error()
     }
 
@@ -38,65 +39,156 @@ async function getXMLResponse(request){
 
 }
 
-async function probeRequest(){
+async function probeRequest() {
     const probeRequest = {
-        method : 'get' , url : `${BASE_URL}/probe`
+        method: 'get', url: `${BASE_URL}/probe`
     }
     const probeResponse = await getXMLResponse(probeRequest)
-    
+
     //get all the categories , to build a object of elements based on categories
     const allCategoryElements = probeResponse.querySelectorAll('[category]')
     const elements = new Set()
-    for(let element of allCategoryElements){
+    for (let element of allCategoryElements) {
         elements.add(element.getAttribute("category"))
     }
 
     //start collecting all the elements based on their categories
     const classifiedElements = {}
-    for(let element of elements){
+    for (let element of elements) {
         const allSelectedElements = probeResponse.querySelectorAll(`[category=${element}]`)
 
         classifiedElements[`${element}`] = []
 
         //we are collecting the ids, for using later to update dom
-        for(let selectedElement of allSelectedElements){
-            classifiedElements[`${element}`].push(selectedElement.getAttribute('id'))    
+        for (let selectedElement of allSelectedElements) {
+            classifiedElements[`${element}`].push(selectedElement.getAttribute('id'))
         }
 
         globalClassifiedElements = classifiedElements
     }
+
+    //set the nextState to current
+    globalNextRequestType = 'current'
 }
 
-async function currentRequest(){
+async function currentRequest() {
     const currentRequest = {
-        method : 'get' , url : `${BASE_URL}/current`
+        method: 'get', url: `${BASE_URL}/current`
     }
     const currentResponse = await getXMLResponse(currentRequest)
-    globalCurrentState = currentResponse
+    globalMachineState = currentResponse
+
+    //extraction of nextSequence no. to keep the loop running 
+    //we will update the globalSequenceNo.
+    let headerElement = currentResponse.querySelector('Header')
+    let nextSequence = headerElement.getAttribute('nextSequence')
+    
+    globalNextSequence = nextSequence
+
+    //set the nextState to sample
+    globalNextRequestType = 'sample'
+
 }
 
-async function sampleRequest(nextSequence){
+async function sampleRequest(nextSequence) {
     const sampleRequest = {
-        method : 'get' , url : `${BASE_URL}/sample?from=${nextSequence}`
+        method: 'get', url: `${BASE_URL}/sample?from=${nextSequence}`
     }
     const sampleResponse = await getXMLResponse(sampleRequest)
 
     //check if the sample response contains some error ? 
     //if it contains OUT_OF_RANGE error -> reset the state of the script
-    
+    let errorElements = sampleResponse.querySelectorAll(`[errorCode]`)
+    if (errorElements !== null) {
+        //errors present lets handle
+        for (let errorElement of errorElements) {
+            let errorCode = errorElement.getAttribute('errorCode')
+            switch (errorCode) {
+                case 'OUT_OF_RANGE':
+                    throw Error(errorElement.textContent)
+                    break
+                default:
+                    throw Error("Fatal Error.")
+                    break
 
-    //start with the DOM Manipulation
-    
-    //helper code below will remove in production
-    const changeIds = []
-    for(let keys in globalClassifiedElements){
-        for(let element of globalClassifiedElements[`${keys}`]){
-            changeIds.push(element)
+            }
         }
+    }
+
+    else {
+        //helper code below will remove in production
+        const changeIds = []
+        for (let keys in globalClassifiedElements) {
+            for (let element of globalClassifiedElements[`${keys}`]) {
+                changeIds.push(element)
+            }
+        }
+
+        //start with the DOM Manipulation
+        //start with the header
+        let originalHeader = globalMachineState.querySelector('Header')
+        originalHeader.parentNode.replaceChild(sampleResponse.querySelector('Header'),originalHeader)
+
+        //begin modifying the body
+        for (let canChangeId of changeIds) {
+            let changeNode = sampleResponse.querySelector(`[dataItemId=${canChangeId}]`)
+            if (changeNode !== null) {
+                let originalNode = globalMachineState.querySelector(`[dataItemId=${canChangeId}]`)
+
+                //using string comparision for now may change in production
+                let serializer = new (globalMachineState.defaultView).XMLSerializer()
+                let changeNodeString = serializer.serializeToString(changeNode)
+                let orignalNodeString = serializer.serializeToString(originalNode)
+
+                if (changeNodeString !== orignalNodeString) {
+                    originalNode.parentNode.replaceChild(changeNode, originalNode)
+                }
+            }
+        }
+
+        //extraction of nextSequence no. to keep the loop running 
+        //we will update the globalSequenceNo.
+        let headerElement = sampleResponse.querySelector('Header')
+        let nextSequence = headerElement.getAttribute('nextSequence')
+        globalNextSequence = nextSequence
+
+        //set the nextState to 'sample'
+        globalNextRequestType = 'sample'
     }
 
 }
 
-probeRequest()
-currentRequest()
-sampleRequest()
+
+async function main() {
+
+
+    //debug
+    let oldMachineState =globalMachineState?(new (globalMachineState.defaultView).XMLSerializer()).serializeToString(globalMachineState):"NULL"
+
+    while (true) {
+        switch (globalNextRequestType) {
+            case 'probe':
+                await probeRequest()
+                break
+            case 'current':
+                await currentRequest()
+                break
+            case 'sample':
+                await sampleRequest(globalNextSequence)
+                break
+            default:
+                break
+        }
+        
+
+        let newMachineState =globalMachineState?(new (globalMachineState.defaultView).XMLSerializer()).serializeToString(globalMachineState):"NULL"
+        if(oldMachineState !== newMachineState){
+            console.log("Changed !!!!!!!!")
+            console.log(newMachineState.toString().slice(0,500))
+        }
+
+        await new Promise((resolve, reject) => setTimeout(resolve, 1000))
+    }
+}
+
+main()
